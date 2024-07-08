0ac60f94fea49a96b1e6931440cf228db48978ac
maxifernandeaz <maximilianofernandezbaez@gmail.com>
2024-07-07 23:02:37 -0300

    se agrego process.env.port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030
 const cors = require ("cors")
 const posteosRouter = require ("./router/posteosRouter.js")
 const db = require ("./data/db.js")
